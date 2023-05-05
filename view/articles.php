c4643b8f2f822f20bfeda1ec95cc2589a84406cd
SylvieCDC <harmonyme.free@gmail.com>
2023-05-05 16:13:28 +0200

    config.php + update  articles.php

--- view/articles.php
+++ view/articles.php
@@ -3,70 +3,69 @@ session_start();
 ?>
 
 <!doctype html>
-
-
 <html lang="fr">
-
-
 <head>
-  <meta charset="utf-8">
-  <title>Titre de la page</title>
-  <script src="script.js"></script>
-  <script src="https://cdn.tailwindcss.com"></script>
-  <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
 
-  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js"></script>
-		
-		<!-- LOADING FONTS AND ICONS -->
-		<link href="http://fonts.googleapis.com/css?family=Poppins:700|Oswald:300" rel="stylesheet" property="stylesheet" type="text/css" media="all">
-		
-		<link rel="stylesheet" type="text/css" href="../assets/css/pe-icon-7-stroke.css">
-		<link rel="stylesheet" type="text/css" href="../assets/css/font-awesome.css">
-		
-		<!-- REVOLUTION STYLE SHEETS -->
-		<link rel="stylesheet" type="text/css" href="../assets/css/settings.css">
-		
-		<!-- FONT AND STYLE FOR BASIC DOCUMENTS, NO NEED FOR FURTHER USAGE IN YOUR PROJECTS-->
-		<link href="http://fonts.googleapis.com/css?family=Roboto%3A700%2C300" rel="stylesheet" property="stylesheet" type="text/css" media="all" />
-		<link rel="stylesheet" type="text/css" href="../assets/css/noneed.css">
-        <link rel="stylesheet" type="text/css" href="../assets/css/accueil.css">
-		
-		<!-- REVOLUTION JS FILES -->
-		<script type="text/javascript" src="../assets/js/jquery.themepunch.tools.min.js"></script>
-		<script type="text/javascript" src="../assets/js/jquery.themepunch.revolution.min.js"></script>
-		
-		<!-- PARTICLES ADD-ON FILES -->
-		<link rel='stylesheet' href='../assets/js/revolution.addon.particles.css?ver=1.0.3' type='text/css' media='all' />
-		<script type='text/javascript' src='../assets/js/revolution.addon.particles.min.js?ver=1.0.3'></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Titre de la page</title>
+    <script src="script.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
 
-		<!-- SLIDER REVOLUTION 5.0 EXTENSIONS  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->	
-		<script type="text/javascript" src="../assets/js/revolution.extension.actions.min.js"></script>
-		<script type="text/javascript" src="../assets/js/revolution.extension.carousel.min.js"></script>
-		<script type="text/javascript" src="../assets/js/revolution.extension.kenburn.min.js"></script>
-		<script type="text/javascript" src="../assets/js/revolution.extension.layeranimation.min.js"></script>
-		<script type="text/javascript" src="../assets/js/revolution.extension.migration.min.js"></script>
-		<script type="text/javascript" src="../assets/js/revolution.extension.navigation.min.js"></script>
-		<script type="text/javascript" src="../assets/js/revolution.extension.parallax.min.js"></script>
-		<script type="text/javascript" src="../assets/js/revolution.extension.slideanims.min.js"></script>
-		<script type="text/javascript" src="../assets/js/revolution.extension.video.min.js"></script>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js"></script>
+		
+    <!-- LOADING FONTS AND ICONS -->
+    <link href="http://fonts.googleapis.com/css?family=Poppins:700|Oswald:300" rel="stylesheet" property="stylesheet" type="text/css" media="all">
+    
+    <link rel="stylesheet" type="text/css" href="../assets/css/pe-icon-7-stroke.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/font-awesome.css">
+    
+    <!-- REVOLUTION STYLE SHEETS -->
+    <link rel="stylesheet" type="text/css" href="../assets/css/settings.css">
+    
+    <!-- FONT AND STYLE FOR BASIC DOCUMENTS, NO NEED FOR FURTHER USAGE IN YOUR PROJECTS-->
+    <link href="http://fonts.googleapis.com/css?family=Roboto%3A700%2C300" rel="stylesheet" property="stylesheet" type="text/css" media="all" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/noneed.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/accueil.css">
+    
+    <!-- REVOLUTION JS FILES -->
+    <script type="text/javascript" src="../assets/js/jquery.themepunch.tools.min.js"></script>
+    <script type="text/javascript" src="../assets/js/jquery.themepunch.revolution.min.js"></script>
+    
+    <!-- PARTICLES ADD-ON FILES -->
+    <link rel='stylesheet' href='../assets/js/revolution.addon.particles.css?ver=1.0.3' type='text/css' media='all' />
+    <script type='text/javascript' src='../assets/js/revolution.addon.particles.min.js?ver=1.0.3'></script>
 
-  <script>
-    tailwind.config = {
-      theme: {
-        extend: {
-          colors: {
-            primary: '#1D1D1F',
-            card: '#1C242A',
-            blanc: '#F1F5F2',
-            rose: '#8c7284',
-            grenat: '#70163c',
-          }
+    <!-- SLIDER REVOLUTION 5.0 EXTENSIONS  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->	
+    <script type="text/javascript" src="../assets/js/revolution.extension.actions.min.js"></script>
+    <script type="text/javascript" src="../assets/js/revolution.extension.carousel.min.js"></script>
+    <script type="text/javascript" src="../assets/js/revolution.extension.kenburn.min.js"></script>
+    <script type="text/javascript" src="../assets/js/revolution.extension.layeranimation.min.js"></script>
+    <script type="text/javascript" src="../assets/js/revolution.extension.migration.min.js"></script>
+    <script type="text/javascript" src="../assets/js/revolution.extension.navigation.min.js"></script>
+    <script type="text/javascript" src="../assets/js/revolution.extension.parallax.min.js"></script>
+    <script type="text/javascript" src="../assets/js/revolution.extension.slideanims.min.js"></script>
+    <script type="text/javascript" src="../assets/js/revolution.extension.video.min.js"></script>
+
+    <script>
+        tailwind.config = {
+        theme: {
+            extend: {
+            colors: {
+                primary: '#1D1D1F',
+                card: '#1C242A',
+                blanc: '#F1F5F2',
+                rose: '#8c7284',
+                grenat: '#70163c',
+            }
+            }
         }
-      }
-    }
-  </script>
+        }
+    </script>
 </head>
-
+<body class="bg-primary">
 <?php
 // On vérifie on est sur quel page
 if (isset($_GET['page']) && !empty($_GET['page'])) {
@@ -97,8 +96,9 @@ $pages = ceil($nbArticles / $parPage);
 $premier = ($currentPage * $parPage) - $parPage;
 
 // On récupère les id, img et nom des films
-$sql = "SELECT id_article, title
-        FROM articles
+$sql = "SELECT images.image_head, articles.id_article, articles.title
+        FROM images
+        INNER JOIN articles ON images.id_article = articles.id_article;
         LIMIT :premier, :parpage";
 $stmt = $pdo->prepare($sql);
 $stmt->bindValue(':premier', $premier, PDO::PARAM_INT);
@@ -106,7 +106,7 @@ $stmt->bindValue(':parpage', $parPage, PDO::PARAM_INT);
 $stmt->execute();
 
 // On stock les films dans un tableau associatif
-$films = $stmt->fetchAll();
+$articles = $stmt->fetchAll();
 
 // var_dump($films);
 // die();
@@ -115,24 +115,22 @@ $films = $stmt->fetchAll();
 ?>
 
 
-
-
     <!--catalogue-->
     <section class="my-9">
         <div class="text-[#EAD7D7] uppercase">
         <div id="result-zone" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
 
                 <!-- On boucle sur les films -->
-                <?php foreach ($films as $film): ?>
+                <?php foreach ($articles as $article): ?>
                     <div class="w-4/5 h-96 mx-auto my-9">
                         <div class="mx-auto my-2 text-center h-96">
 
                             <!-- On envoie vers le film avec l'id en paramètres GET -->
-                            <a href="content/pages/film.php?id=<?php echo $film['id_film']; ?>">
+                            <a href="content/pages/film.php?id=<?php echo $article['id_article']; ?>">
                                 <!-- On affiche ce qu'on veut afficher du film, et qu'on récupère de la db -->
-                                <img class="h-96 mx-auto" src="<?php echo $film['img_film'] ?>" alt="Affiche" />
+                                <img class="h-96 mx-auto" src="<?php echo $article['image_head'] ?>" alt="article" />
                                 <span>
-                                    <?php echo $film['nom_film'] ?>
+                                    <?php echo $article['title'] ?>
                                 </span>
                             </a>
                         </div>
@@ -183,7 +181,7 @@ $films = $stmt->fetchAll();
     <!---->
 
 
-    </body>
+</body>
 
 
 </html>
