5fc6ea3adada10d26e23f3c88b3beb727d36e199
wJhayyy <skytek077@gmail.com>
2023-05-12 14:15:01 +0200

    navbarv2 + card margin

--- view/includes/navbar.php
+++ view/includes/navbar.php
@@ -1,71 +1,70 @@
-<div class="w-full">
+<div class="hidden sticky top-0 z-50 md:w-full relative md:block">
+    
     <nav class="bg-primary">
-      <div class="container flex justify-center w-fit min-w-full">
-        <a href="#"><img class="md:hidden w-4/12" src="assets/images/logowithoutback.png"></a>
-
-        <button data-collapse-toggle="mobile-menu" type="button"
-          class="md:hidden ml-3 text-gray-400 hover:text-blanc focus:outline-none focus:ring-2 focus:ring-blue-300 rounded-lg inline-flex items-center justify-center"
-          aria-controls="mobile-menu-2" aria-expanded="false">
-          <span class="sr-only">Open main menu</span>
-          <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"></path>
-          </svg>
-          <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clip-rule="evenodd"></path>
-          </svg>
+    <div class="container mx-auto flex flex-wrap items-center justify-between w-fit md:container flex justify-center w-fit min-w-full">
+      <a href="#"><img class="md:hidden w-4/12 ml-6" src="assets/images/logowithoutback.png"></a>
+        <button data-collapse-toggle="mobile-menu" type="button" class="md:hidden mr-3 text-gray-400 hover:text-blanc focus:outline-none focus:ring-2 focus:ring-blue-300 rounded-lg inline-flex items-center justify-center" aria-controls="mobile-menu-2" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+        <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
         </button>
-
         <div class="hidden md:block w-full" id="mobile-menu">
-          <ul
-            class="flex-col items-center justify-around md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
-            <li>
-              <a href="#"
-                class="flex items-center text-blanc hover:text-primary hover:bg-blanc duration-300 border-b border-gray-100 md:hover:bg-blanc md:border-0 pl-3 pr-4 py-2 md:hover:text-primary md:rounded-lg p-2 mt-1 font-medium md:w-auto"
-                aria-current="page">Articles</a>
+        <ul class="flex-col items-center justify-around md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
+            <li class="w-full md:w-auto">
+            <a href="articles.php" class="flex items-center md:navbaranim " aria-current="page">Articles</a>
             </li>
-            <li>
-              <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
-                class="flex items-center text-blanc font-medium hover:text-primary hover:bg-blanc border-b border-gray-100 md:hover:bg-blanc md:border-0 pl-3 pr-4 py-2 md:hover:text-primary md:rounded-lg p-2 mt-1 md:font-medium md:w-auto">Présentations
-                <svg class="w-4 h-4 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clip-rule="evenodd"></path>
-                </svg></button>
-              <!-- Dropdown menu -->
-              <div id="dropdownNavbar"
-                class="hidden bg-white text-base z-10 list-none divide-y divide-gray-100 rounded shadow my-4 w-44">
-                <ul class="py-1" aria-labelledby="dropdownLargeButton">
-                  <li>
-                    <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Dashboard</a>
-                  </li>
-                  <li>
-                    <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Settings</a>
-                  </li>
-                  <li>
-                    <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Earnings</a>
-                  </li>
-                </ul>
-                <div class="py-1">
-                  <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Sign out</a>
+            <li class="w-full md:w-auto">
+                <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center text-blanc font-medium w-full md:navbaranim">Présentations <svg class="w-4 h-4 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></button>
+                <!-- Dropdown menu -->
+                <div id="dropdownNavbar" class="hidden bg-white text-base z-10 list-none divide-y divide-gray-100 rounded shadow my-4 w-44">
+                    <ul class="" aria-labelledby="dropdownLargeButton">
+                    <li>
+                        <a href="presentation.php" class="bg-primary text-blanc hover:bg-blanc hover:text-primary block px-4 py-2">Nos compétences</a>
+                    </li>
+                    <li>
+                        <a href="presentation.php" class="bg-primary text-blanc hover:bg-blanc hover:text-primary block px-4 py-2">Notre équipe</a>
+                    </li>
+                    </ul>
                 </div>
-              </div>
             </li>
             <li class="hidden md:block w-2/12">
-              <a href="#"><img class="w-4/6" src="assets/images/logowithoutback.png"></a>
+           <a href="#"><img class="w-4/6" src="assets/images/logowithoutback.png"></a>
             </li>
-            <li>
-              <a href="#"
-                class="font-medium text-white hover:text-primary hover:bg-white border-b border-gray-100 md:hover:bg-white md:border-0 block pl-3 pr-4 py-2 md:hover:text-primary md:rounded-lg p-2 mt-1">Réalisations</a>
+            <li class="w-full md:w-auto">
+            <a href="realisations.php" class="font-medium text-blanc md:navbaranim">Réalisations</a>
             </li>
-            <li>
-              <a href="#"
-                class="font-medium text-white hover:text-primary hover:bg-white border-b border-gray-100 md:hover:bg-white md:border-0 block pl-3 pr-4 py-2 md:hover:text-primary md:rounded-lg p-2 mt-1">Contact</a>
+            <li class="w-full md:w-auto">
+            <a href="#contacternous" class="font-medium text-blanc md:navbaranim">Contact</a>
             </li>
-          </ul>
+        </ul>
         </div>
+    </div>
     </nav>
-  </div>
+</div>
+
+
+<div class="flex w-full md:hidden" id="nav">
+    <a href="index.php" class="button active">
+        <i class="fa-solid fa-house"></i> 
+    </a>
+    <a href="#contacternous" class="button">
+        <i class="fa-solid fa-envelope"></i>
+    </a>
+    <a href="articles.php" class="button">
+        <i class="fa-solid fa-newspaper"></i>
+    </a>
+    <button type="button" class="button" data-dropdown-toggle="dropupPhone">
+        <i class="fa-solid fa-bars"></i>
+    </button>
+	<div id="dropupPhone" class="hidden bg-white text-base z-10 list-none divide-y divide-gray-100 rounded shadow my-4 w-44">
+    <ul class="py-1 bg-primary" aria-labelledby="dropdownLargeButton">
+        <li>
+            <a href="realisations.php" class="text-blanc bg-primary block px-4 py-2">Réalisation</a>
+        </li>
+        <li>
+            <a href="presentation.php" class="text-blanc bg-primary block px-4 py-2">Présentation</a>
+        </li>
+    </ul>
+</div>
+
+</div>
