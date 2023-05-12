7ff51f35bd6865b37f1737b337e645a5eaf60c1c
body08120 <nataelrenollet08120@gmail.com>
2023-05-12 14:17:05 +0200

    nav

--- view/includes/navbar.php
+++ view/includes/navbar.php
@@ -20,7 +20,7 @@
                 <div id="dropdownNavbar" class="hidden bg-white text-base z-10 list-none divide-y divide-gray-100 rounded shadow my-4 w-44">
                     <ul class="" aria-labelledby="dropdownLargeButton">
                     <li>
-                        <a href="#" class="bg-primary text-blanc hover:bg-blanc hover:text-primary block px-4 py-2">Nos compétences</a>
+                        <a href="index.php?action=presentation#sequence" class="bg-primary text-blanc hover:bg-blanc hover:text-primary block px-4 py-2">Nos compétences</a>
                     </li>
                     <li>
                         <a href="#" class="bg-primary text-blanc hover:bg-blanc hover:text-primary block px-4 py-2">Notre équipe</a>
