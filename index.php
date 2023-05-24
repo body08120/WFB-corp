d526f383b0fa99c9da034628259a6425bd7dc038
SylvieCDC <harmonyme.free@gmail.com>
2023-05-24 16:12:30 +0200

    update index.php, accueil.php (view),

--- index.php
+++ index.php
@@ -1,33 +1,42 @@
 <?php
 session_start();
-require_once('src/controller/accueil.php');
-require_once('src/controller/presentation.php');
-require_once('src/controller/articles.php');
-require_once('src/controller/article.php');
-require_once('src/controller/realisations.php');
-
+// require_once('src/controller/accueil.php');
+// require_once('src/controller/presentation.php');
+// require_once('src/controller/articles.php');
+// require_once('src/controller/article.php');
+// require_once('src/controller/realisations.php');
 
 if (isset($_GET['action']) && $_GET['action'] !== '') {
     switch ($_GET['action']) {
         case 'presentation':
+            require_once('src/controller/presentation.php');
             presentationVue();
             break;
         case 'articles':
+            require_once('src/controller/articles.php');
             articlesVue();
             break;
         case 'article':
+            require_once('src/controller/article.php');
             articleVue();
             break;
         case 'realisations':
+            require_once('src/controller/realisations.php');
             realisationsVue();
             break;
         // génère la home si aucun des cases n'est trouvé
         default:
-            homepage();
+            require_once('view/accueil.php');
             break;
     }
 } else {
-    // Génère la home
-    homepage();
+    if(!empty($_POST))
+    {
+        // Déclencher le nouveau script du formulaire de traitement
+    }
+    else
+    {
+        require_once('view/accueil.php');
+    }
 }
 ?>
--- view/accueil.php
+++ view/accueil.php
@@ -918,31 +918,51 @@
               class="bg-blanc bg-opacity-10 backdrop-blur block rounded-lg shadow-lg px-6 py-12 md:px-12 lg:-mr-14"
             >
             <h2 class="text-3xl font-bold mb-12">Nous contacter</h2>
-            <form>
+            <form action="src/controller/contact_ttt.php" method="post">
               <div class="form-group mb-6">
                 <input
                   type="text"
                   class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                   id="exampleInput7"
                   placeholder="Nom / Prénom"
+				  name="nom-user"
                 />
               </div>
               <div class="form-group mb-6">
-                <input
+				  <input
                   type="email"
                   class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                   id="exampleInput8"
                   placeholder="Adresse Email"
-                />
-              </div>
+				  name="mail-user"
+				  />
+				</div>
+				<div class="form-group mb-6">
+				  <input
+					type="text"
+					class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+					id="exampleInput7"
+					placeholder="Sujet"
+					name="motif"
+				  />
+				</div>
               <div class="form-group mb-6">
                 <textarea
                   class="resize-none form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                   id="exampleFormControlTextarea13"
                   rows="3"
                   placeholder="Votre message"
+				  name="message"
                 ></textarea>
               </div>
+
+			  <!-- champ caché pour générer des jetons -->
+			  <div class="form-group mb-6">
+			  	<input
+				 type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
+
+			  </div> 
+
               <button
                 type="submit"
                 class="w-full px-6 py-2.5 bg-grenat text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-rose hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
