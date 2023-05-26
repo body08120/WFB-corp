c060f4c15775055a9639afa0ba9132f27821099e
SylvieCDC <harmonyme.free@gmail.com>
2023-05-26 09:52:56 +0200

    login et signup form

--- src/model/classes/User.php
+++ src/model/classes/User.php
@@ -5,19 +5,19 @@ require_once 'src/model/config.php';
 
 class User extends Connect
 {
-    private $pseudo;
+    private $firstname;
     private $nom;
-    private $prenom;
+    private $email;
     private $password;
     private $code;
 
     // Définit les propriétés de la classe User
 
-    public function __construct($pseudo, $nom, $prenom, $password, $code)
+    public function __construct($firstname, $nom, $email, $password, $code)
     {
-        $this->pseudo = $pseudo;
+        $this->firstname = $firstname;
         $this->nom = $nom;
-        $this->prenom = $prenom;
+        $this->email = $email;
         $this->password = $password;
         $this->code = $code;
     }
@@ -35,21 +35,21 @@ class User extends Connect
         }
         // Valide les champs du formulaire en utilisant la méthode validateFields() et retourne false si la validation échoue
 
-        // Vérifier si le pseudo est déjà utilisé
-        if ($this->isPseudoTaken($db)) {
+        // Vérifier si le firstname est déjà utilisé
+        if ($this->isfirstnameTaken($db)) {
             return false;
         }
-        // Vérifie si le pseudo est déjà utilisé en utilisant la méthode isPseudoTaken() et retourne false si le pseudo est pris
+        // Vérifie si le firstname est déjà utilisé en utilisant la méthode isfirstnameTaken() et retourne false si le firstname est pris
 
         // Hasher le mot de passe
         $hashedPassword = password_hash($this->password, PASSWORD_DEFAULT);
         // Hash le mot de passe en utilisant la fonction password_hash() avec l'algorithme PASSWORD_DEFAULT
 
         // Insérer l'utilisateur dans la base de données
-        $stmt = $db->prepare("INSERT INTO utilisateurs (pseudo, nom, prenom, mot_de_passe, code_admin) VALUES (:pseudo, :nom, :prenom, :password, :code)");
-        $stmt->bindValue(':pseudo', $this->pseudo);
+        $stmt = $db->prepare("INSERT INTO users (firstname, nom, email, mot_de_passe, code_admin) VALUES (:firstname, :nom, :email, :password, :code)");
+        $stmt->bindValue(':firstname', $this->firstname);
         $stmt->bindValue(':nom', $this->nom);
-        $stmt->bindValue(':prenom', $this->prenom);
+        $stmt->bindValue(':email', $this->email);
         $stmt->bindValue(':password', $hashedPassword);
         $stmt->bindValue(':code', $this->code);
         $stmt->execute();
@@ -61,7 +61,7 @@ class User extends Connect
     private function validateFields()
     {
         // Valider que tous les champs du formulaire sont renseignés
-        if (empty($this->pseudo) || empty($this->nom) || empty($this->prenom) || empty($this->password) || empty($this->code)) {
+        if (empty($this->firstname) || empty($this->nom) || empty($this->email) || empty($this->password) || empty($this->code)) {
             return false;
         }
         // Vérifie si tous les champs du formulaire sont renseignés et retourne false si l'un des champs est vide
@@ -77,19 +77,19 @@ class User extends Connect
         return true;
     }
 
-    private function isPseudoTaken($db)
+    private function isfirstnameTaken($db)
     {
-        $stmt = $db->prepare("SELECT COUNT(*) as count FROM utilisateurs WHERE pseudo = :pseudo");
-        $stmt->bindValue(':pseudo', $this->pseudo);
+        $stmt = $db->prepare("SELECT COUNT(*) as count FROM utilisateurs WHERE firstname = :firstname");
+        $stmt->bindValue(':firstname', $this->firstname);
         $stmt->execute();
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
-        // Prépare et exécute une requête pour compter le nombre d'utilisateurs avec le même pseudo
+        // Prépare et exécute une requête pour compter le nombre d'utilisateurs avec le même firstname
 
         if ($result['count'] > 0) {
-            return true; // Le pseudo est déjà utilisé
+            return true; // Le firstname est déjà utilisé
         }
 
-        return false; // Le pseudo est disponible
+        return false; // Le firstname est disponible
     }
 
     public function authenticate()
@@ -104,11 +104,11 @@ class User extends Connect
         // Valide les champs du formulaire en utilisant la méthode validateFields() et retourne false si la validation échoue
 
         // Requête préparée pour rechercher l'utilisateur
-        $stmt = $db->prepare("SELECT mot_de_passe FROM utilisateurs WHERE pseudo = :pseudo");
-        $stmt->bindValue(':pseudo', $this->pseudo);
+        $stmt = $db->prepare("SELECT mot_de_passe FROM utilisateurs WHERE firstname = :firstname");
+        $stmt->bindValue(':firstname', $this->firstname);
         $stmt->execute();
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
-        // Prépare et exécute une requête pour obtenir le mot de passe de l'utilisateur correspondant au pseudo
+        // Prépare et exécute une requête pour obtenir le mot de passe de l'utilisateur correspondant au firstname
 
         if (!$result || !password_verify($this->password, $result['mot_de_passe'])) {
             return false; // L'authentification a échoué
@@ -121,15 +121,15 @@ class User extends Connect
 
 // Vérifier si le formulaire d'inscription a été soumis
 if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["register"])) {
-    $pseudo = isset($_POST["pseudo"]) ? trim($_POST["pseudo"]) : '';
+    $firstname = isset($_POST["firstname"]) ? trim($_POST["firstname"]) : '';
     $nom = isset($_POST["nom"]) ? trim($_POST["nom"]) : '';
-    $prenom = isset($_POST["prenom"]) ? trim($_POST["prenom"]) : '';
+    $email = isset($_POST["email"]) ? trim($_POST["email"]) : '';
     $password = isset($_POST["mdp"]) ? $_POST["mdp"] : '';
     $verifmdp = isset($_POST["verifmdp"]) ? $_POST["verifmdp"] : '';
     $code = isset($_POST["code"]) ? trim($_POST["code"]) : '';
     // Récupère les valeurs des champs du formulaire soumis en utilisant isset() et trim() pour nettoyer les espaces
 
-    $user = new User($pseudo, $nom, $prenom, $password, $code);
+    $user = new User($firstname, $nom, $email, $password, $code);
     // Crée une nouvelle instance de la classe User en passant les valeurs récupérées du formulaire
 
     if ($user->register()) {
@@ -142,11 +142,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["register"])) {
 
 // Vérifier si le formulaire d'authentification a été soumis
 if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["login"])) {
-    $pseudo = isset($_POST["pseudo"]) ? $_POST["pseudo"] : '';
+    $firstname = isset($_POST["firstname"]) ? $_POST["firstname"] : '';
     $password = isset($_POST["mdp"]) ? $_POST["mdp"] : '';
     // Récupère les valeurs des champs du formulaire soumis en utilisant isset()
 
-    $user = new User($pseudo, '', '', $password, '');
+    $user = new User($firstname, '', '', $password, '');
     // Crée une nouvelle instance de la classe User en passant les valeurs récupérées du formulaire
 
     if ($user->authenticate()) {
