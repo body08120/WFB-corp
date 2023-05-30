218d1044e0c173606fad2d1460f172585f74c08a
body08120 <nataelrenollet08120@gmail.com>
2023-05-30 20:56:25 +0200

    paneau admin

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 require_once('src/controller/homeController.php');
+require_once('src/controller/adminController.php');
+
 
 
 if (isset($_GET['action']) && $_GET['action'] !== '') {
@@ -21,9 +23,18 @@ if (isset($_GET['action']) && $_GET['action'] !== '') {
             vueRealisations();
             break;
         // génère la home si aucun des cases n'est trouvé
-        default:
-            homepage();
-            break;
+    }
+} elseif (isset($_GET['admin']) && $_GET['admin'] !== '') {
+    //role rentrer en dur pour acceder panel admin
+    $_SESSION['role_user'] = 2;
+    if (isset($_SESSION['role_user'])) {
+        switch ($_GET['admin']) {
+            case 'administration':
+                panelAdmin();
+                break;
+        }
+    } else {
+        homepage();
     }
 } else {
     if (!empty($_POST['contact'])) {
