dddae20f737d4b0585f12c04b5aaf3af4acdcb7e
wJhayyy <skytek077@gmail.com>
2023-06-07 10:32:30 +0200

    cards articles

--- view/articles.php
+++ view/articles.php
@@ -92,7 +92,7 @@ $articles_ref = $stmt_ref->fetchAll(PDO::FETCH_ASSOC);
     ?>
 
     <main class="h-full overflow-hidden flex items-center justify-center">
-        <div class="space-y-5 w-10/12">
+        <div class="space-y-5 w-full">
             <div class="overflow-hidden rounded-xl bg-[#141414]-50 p-1 mt-6">
                 <ul class="flex items-center gap-2 text-sm font-medium ">
                     <li class="l flex-1">
