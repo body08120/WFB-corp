4a553fd7e32545da679997b760427e5b20f04ab4
wJhayyy <skytek077@gmail.com>
2023-06-07 10:39:24 +0200

    cards accueil

--- view/realisations.php
+++ view/realisations.php
@@ -87,9 +87,9 @@ $projet_application = $stmt_app->fetchAll(PDO::FETCH_ASSOC);
     <?php include_once('includes/navbar.php') ?>
 
     <main class="h-full overflow-hidden flex items-center justify-center">
-        <div class="space-y-5 w-10/12">
+        <div class="space-y-5 w-full">
 
-            <div class="overflow-hidden rounded-xl p-1 mt-6">
+            <div class="overflow-hidden rounded-xl bg-[#141414]-50 p-1 mt-6">
                 <ul class="flex items-center gap-2 text-sm font-medium ">
                     <li class="l flex-1">
                         <a href="#all-section"
